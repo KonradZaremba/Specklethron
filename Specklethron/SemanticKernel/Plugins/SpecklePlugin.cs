b48252e689f5555f2827c3f008883c4b9b2eddcc
Konrad Zaremba <konradzaremba@gmail.com>
2024-06-08 00:41:54 +0200

    skills moved

---
+++ Specklethron/SemanticKernel/Plugins/SpecklePlugin.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.SemanticKernel;
+using Specklethron.Speckle;
+using System.ComponentModel;
+using Stream = Speckle.Core.Api.Stream;
+
+
+namespace Specklethron.Plugins
+{
+    public class SpeklePlugin
+    {
+
+        [KernelFunction, Description("Check accounts you can login, give user names")]
+        public async Task<List<string>> GetUsers()
+        {
+            try
+            {
+                return await Task.Run(() => SpeckleConnector.GetUsers());
+
+            }
+            catch
+            {
+                return new List<string>() { "No user logged in go to youe Spekle manager" };
+            }
+        }
+
+        [KernelFunction, Description("Login to account with given user name")]
+        public async Task<string> Login([Description("User name")] string userName)
+        {
+            try
+            {
+                var users = await SpeckleConnector.Login(userName);
+                return users;
+            }
+            catch
+            {
+                return "Couldn't login";
+            }
+        }
+
+        [KernelFunction, Description("Get user streams")]
+        public async Task<List<Stream>> GetSterams()
+        {
+            try
+            {
+                return await SpeckleConnector.GetSterams();
+            }
+            catch
+            {
+                throw new ArgumentException("Couldn't fetch streams");
+            }
+        }
+
+        [KernelFunction, Description("Get user stream")]
+        public async Task<Stream> GetSteram([Description("Stram Id")] string id)
+        {
+            try
+            {
+                return await SpeckleConnector.GetSteram(id);
+            }
+            catch
+            {
+                throw new ArgumentException("Couldn't fetch stream");
+            }
+        }
+    }
+}
---
+++ Specklethron/Speckle/SpeckleConnector.cs
@@ -0,0 +1,41 @@
+﻿using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Stream = Speckle.Core.Api.Stream;
+
+namespace Specklethron.Speckle
+{
+    public class SpeckleConnector
+    {
+        public static Client _client;
+
+        public async static Task<string> Login(string username)
+        {
+            var account = AccountManager.GetAccounts().Where(x => x.userInfo.name == username).SingleOrDefault();
+
+            if (account is null) return "fail to login";
+            _client = new Client(account);
+
+            return ("Logged in:" + _client.Account.userInfo.name);
+        }
+
+        public static List<string> GetUsers()
+        {
+            var accounts = AccountManager.GetAccounts();
+            return accounts.Select(x => x.userInfo.name).ToList();
+        }
+
+        public async static Task<List<Stream>> GetSterams()
+        {
+            if (_client is null) throw new ArgumentException("you need to login first");
+            return await _client.StreamsGet();
+        }
+
+        public async static Task<Stream> GetSteram(string id)
+        {
+            var cts = new CancellationTokenSource();
+
+            if (_client is null) throw new ArgumentException("you need to login first");
+            return await _client.StreamGet(id);
+        }
+    }
+}
